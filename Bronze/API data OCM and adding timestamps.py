f6549dccaa83dcd3cbf9bd7ab7e925be5c5d411b
Adam Willner <adamwillner2@gmail.com>
2026-06-29 13:29:58 +0200

    Added timestamps

---
+++ Bronze/API data OCM and adding timestamps.py
@@ -0,0 +1,73 @@
+#%%
+import requests
+import pandas as pd
+from pathlib import Path
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+BASE_DIR = Path.cwd()
+OUTPUT_DIR = BASE_DIR / "Bronze" 
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+load_dotenv()
+
+OPENCHARGEMAP_API_KEY = os.getenv("OPENCHARGEMAP_API_KEY")
+
+#%%
+url = "https://api.openchargemap.io/v3/poi"
+
+params = {
+    "output": "json",
+    "countrycode": "FI",
+    "maxresults": 100000,
+    "key": OPENCHARGEMAP_API_KEY
+}
+
+response = requests.get(url, params=params)
+
+print(response.status_code)
+
+data = response.json()
+
+open_charge_raw_FI = pd.json_normalize(data)
+
+open_charge_raw_FI["fetch_timestamp"] = pd.Timestamp.now('UTC')
+
+open_charge_raw_FI.to_csv(OUTPUT_DIR / 'open_charge_raw_FI.csv', index=False)
+
+# %%
+
+# import swedish data
+url = "https://api.openchargemap.io/v3/poi"
+
+params = {
+    "output": "json",
+    "countrycode": "SE",
+    "maxresults": 100000,
+    "key": OPENCHARGEMAP_API_KEY
+}
+
+response = requests.get(url, params=params)
+
+print(response.status_code)
+
+data = response.json()
+
+open_charge_raw_SE = pd.json_normalize(data)
+
+open_charge_raw_SE["fetch_timestamp"] = pd.Timestamp.now('UTC')
+
+open_charge_raw_FI.to_csv(OUTPUT_DIR / 'open_charge_raw_SE.csv', index=False)
+
+#%%
+
+new_car_registrations_finland = pd.read_csv('new_car_registrations_finland.csv',  encoding="latin1")
+new_car_registrations_sweden = pd.read_csv('new_car_registrations_sweden.csv',  encoding="latin1")
+
+new_car_registrations_finland["fetch_timestamp"] = pd.Timestamp.now('UTC')
+new_car_registrations_sweden["fetch_timestamp"] = pd.Timestamp.now('UTC')
+
+new_car_registrations_finland.to_csv('new_car_registrations_finland.csv',  encoding="utf-8", index=False)
+new_car_registrations_sweden.to_csv('new_car_registrations_sweden.csv',  encoding="utf-8", index=False)
+# %%
